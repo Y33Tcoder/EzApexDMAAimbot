b2c8a16c0b73bf764ef4b9c40b4f4ebb4b195acf
Y33Tcoder <rc1998902@gmail.com>
2020-06-26 07:17:34 +0800

    Lost Treasures offset update

--- vmread/offsets.h
+++ vmread/offsets.h
@@ -1,8 +1,8 @@
 #pragma once
 //Offsets
-#define OFFSET_ENTITYLIST			0x1767348
-#define OFFSET_LOCAL				0x1038934
-#define OFFSET_LOCAL_ENT			0x1CCDFF8 //(grapple_swingPullAngle+0xA8)0x022E93F8
+#define OFFSET_ENTITYLIST			0x175dc28
+//#define OFFSET_LOCAL				0x1B0C3A8
+#define OFFSET_LOCAL_ENT			0x1b0c3a8 //(grapple_swingPullAngle+0xA8)0x022E93F8
 
 
 #define OFFSET_TEAM					0x3F0
@@ -18,13 +18,13 @@
 #define OFFSET_CAMERAPOS			0x1DA4
 #define OFFSET_VIEWANGLES			0x23C8
 #define OFFSET_OBSERVING_TARGET		0x3210
-#define OFFSET_BLEED_OUT_STATE      0x2588
+#define OFFSET_BLEED_OUT_STATE      0x2590
 
 #define OFFSET_BULLET_SPEED         0x1D48
 #define OFFSET_BULLET_GRAVITY       0x1D50
 
 #define OFFSET_MATRIX 0x1b3bd0
-#define OFFSET_RENDER 0x3f3aad8
+#define OFFSET_RENDER 0x3f5a970
 
 
 //W2S pattern (view render and view matrix) 48 89 ?? ?? ?? 57 48 83 EC ?? 48 8B D9 48 8B FA 48 8B 0D ?? ?? ?? ?? 33 D2 48 8B 01 FF
